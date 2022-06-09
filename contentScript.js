549e6636e01f9c92873d1937b7a1a2aa5a8df389
tahid02 <tanzim1463@gmail.com>
2022-06-09 15:59:09 +0600

    preset selection in input done

--- contentScript.js
+++ contentScript.js
@@ -1,50 +1,66 @@
 (() => {
-  // show all presets on action clicked
-
   // create the div for preset element
   let main = document.getElementsByClassName("GutterDivStyled-sc-19aqteq-0")[5];
-  console.log(main);
-  //   main.innerText = "hello";
-  //   let show = false;
 
   const dropdownDiv = document.createElement("div");
-  const selectPresetBtn = document.createElement("button");
-  selectPresetBtn.innerText = "select preset from here";
-  const presetNameListUl = document.createElement("ul");
-  presetNameListUl.style.position = "absolute";
-
-  //   const presetLi = document.createElement("li");
-  dropdownDiv.appendChild(selectPresetBtn);
-  dropdownDiv.appendChild(presetNameListUl);
+  const coverLetter = document.createElement("input");
   main.prepend(dropdownDiv);
-  presetNameListUl.addEventListener("click", function (e) {
+  main.prepend(coverLetter);
+  // style="position: absolute;z-index:100;width:10rem
+  dropdownDiv.innerHTML = `
+ <div class="select-menu ">
+   <div class="select-btn">
+     <span class="sBtn-text">Select your option</span>
+     <i class="bx bx-chevron-down"></i>
+   </div>
+ 
+   <ul class="options" style="height:30vh;overflow-y:scroll" >
+  
+     
+   </ul>
+ </div>`;
+
+  const optionMenu = document.querySelector(".select-menu"),
+    selectBtn = optionMenu.querySelector(".select-btn"),
+    optionsUl = optionMenu.querySelector(".options"),
+    sBtn_text = optionMenu.querySelector(".sBtn-text");
+  console.log("opt", optionsUl);
+  selectBtn.addEventListener("click", () =>
+    optionMenu.classList.toggle("active")
+  );
+  optionsUl.addEventListener("click", (e) => {
+    sBtn_text.innerText = e.target.innerText;
+    optionMenu.classList.remove("active");
     console.log(e.target.innerText);
-  });
-  presetNameListUl.style.display = "none";
-  selectPresetBtn.addEventListener("click", function () {
-    presetNameListUl.style.display = "block";
+
+    chrome.storage.sync.get("allPresets", (obj) => {
+      if (obj.allPresets) {
+        console.log(obj.allPresets);
+        let cover = obj.allPresets.filter(
+          (preset) => preset.presetName === e.target.innerText
+        );
+        console.log({ cover });
+        coverLetter.value = cover[0].presetDescription;
+      }
+    });
   });
 
-  console.log(presetNameListUl);
+  console.log(main);
+
   chrome.storage.sync.get("allPresets", (obj) => {
     if (obj.allPresets) {
-      console.log("show drpdown", obj.allPresets);
-      // show presets in dropdown list
+      console.log(obj.allPresets);
       obj.allPresets.map((preset) => {
         const { presetName, presetDescription } = preset;
-        console.log(presetName);
-        console.log(presetDescription);
-        let itemBtn = document.createElement("button");
-        itemBtn.innerText = presetName;
-
-        itemBtn.addEventListener("click", function () {
-          presetNameListUl.style.display = "none";
-        });
-        presetNameListUl.appendChild(itemBtn);
+        let itemLi = document.createElement("li");
+        itemLi.className = "option";
+        itemLi.innerHTML = `<span class="option-text">${presetName}</span>`;
+        optionsUl.appendChild(itemLi);
+        console.log(main);
       });
     } else {
       // show 'no preset available text' in dropdown
-      presetNameListUl.innerText = "no preset available now";
+      optionsUl.innerText = "no preset available now";
     }
   });
 })();
