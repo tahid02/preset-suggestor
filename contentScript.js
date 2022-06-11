38eac903aaeb204741ec7d90e4561e68928859e1
tahid02 <tanzim1463@gmail.com>
2022-06-11 17:31:53 +0600

    basic func done

--- contentScript.js
+++ contentScript.js
@@ -1,83 +1,95 @@
 (() => {
   window.addEventListener("load", myMain, false);
+  // create dropdown div
+  function createDropdownDiv() {
+    const dropdownDiv = document.createElement("div");
+
+    dropdownDiv.innerHTML = `
+        <div style="display: flex; justify-content: space-between;padding-bottom:1rem" id="ext_textDropdown">
+            <div 
+              style="display: flex;
+                    align-items: flex-end;  
+                    font-size: .9rem;
+                    font-weight: 500;">
+              <div>Cover Letter</div>
+            </div>
+            <div class="select-menu ">
+                <div class="select-btn" style="background-color:#FAC213;border:1px solid black">
+                  <span class="sBtn-text">Select a cover letter preset</span>
+                    <img src="https://www.iconpacks.net/icons/2/free-arrow-down-icon-3101-thumb.png" style="width:1rem" alt="arrow" />
+                </div>
+
+                <ul class="options" style="height:30vh;overflow-y:scroll;position:absolute" >
+
+                </ul>
+              </div>
+            </div>
+     `;
+    return dropdownDiv;
+  }
   function myMain(evt) {
     var jsInitChecktimer = setInterval(checkForJS_Finish, 1000);
 
     function checkForJS_Finish() {
-      let area = document.getElementsByClassName("cover-letter-area")[0];
-      console.log("first", typeof area);
-      if (area) {
-        console.log("hello bitch");
-        console.log("dom loaded");
-
-        // create the div for preset element
-
-        console.log("mahtab says hello");
-        console.log(area);
+      let coverArea = document.getElementsByClassName("cover-letter-area")[0];
+      // let coverArea = document.getElementsByTagName("label")[3];
+      console.log("first", typeof coverArea);
+      console.log("cov", coverArea);
+      if (coverArea) {
+        // stop searching for cover letter area element
         clearInterval(jsInitChecktimer);
-        const coverLetter = document.createElement("input");
-        area.appendChild(coverLetter);
+        // now , create dropdown
+        const dropdown = createDropdownDiv();
+        // append this dropdown in the found cover letter area element
+        coverArea.prepend(dropdown);
+        document.getElementById("cover_letter_label").remove();
+
+        const optionMenu = document.querySelector(".select-menu"),
+          selectBtn = optionMenu.querySelector(".select-btn"),
+          optionsUl = optionMenu.querySelector(".options"),
+          sBtn_text = optionMenu.querySelector(".sBtn-text");
+
+        console.log(optionsUl);
+        // toggle select btn
+        selectBtn.addEventListener("click", () =>
+          optionMenu.classList.toggle("active")
+        );
+        // show all the available preset in option list
+        chrome.storage.sync.get("allPresets", (obj) => {
+          if (obj.allPresets) {
+            console.log(obj.allPresets);
+            obj.allPresets.map((preset) => {
+              let itemLi = document.createElement("li");
+              itemLi.className = "option";
+              itemLi.innerHTML = `<span class="option-text">${preset.presetName}</span>`;
+              optionsUl.appendChild(itemLi);
+            });
+          } else {
+            // show 'no preset available text' in dropdown
+            optionsUl.innerText = "no preset available ";
+          }
+        });
+
+        // show selected preset cover letter in input field
+        optionsUl.addEventListener("click", (e) => {
+          sBtn_text.innerText = e.target.innerText;
+          optionMenu.classList.remove("active");
+          console.log(e.target.innerText);
+          // filter the cover letter by selected preset name
+          chrome.storage.sync.get("allPresets", (obj) => {
+            if (obj.allPresets) {
+              console.log(obj.allPresets);
+              let cover = obj.allPresets.filter(
+                (preset) => preset.presetName === e.target.innerText
+              );
+              console.log({ cover });
+              const coverTextArea = document.getElementsByTagName("textarea");
+              console.log(coverTextArea);
+              coverTextArea[0].value = cover[0].presetDescription;
+            }
+          });
+        });
       }
     }
   }
-  // let main = document.querySelector(".cover-letter-area");
-  // console.log("loaded", main);
-  // const dropdownDiv = document.createElement("div");
-  // const coverLetter = document.createElement("input");
-
-  // dropdownDiv.innerHTML = `
-  //     <div class="select-menu ">
-  //       <div class="select-btn">
-  //         <span class="sBtn-text">Select your option</span>
-  //         <i class="bx bx-chevron-down"></i>
-  //       </div>
-
-  //       <ul class="options" style="height:30vh;overflow-y:scroll" >
-
-  //       </ul>
-  //     </div>`;
-  // const optionMenu = document.querySelector(".select-menu"),
-  //   selectBtn = optionMenu.querySelector(".select-btn"),
-  //   optionsUl = optionMenu.querySelector(".options"),
-  //   sBtn_text = optionMenu.querySelector(".sBtn-text");
-  // console.log("opt", optionsUl);
-  // // toggle select btn
-  // selectBtn.addEventListener("click", () =>
-  //   optionMenu.classList.toggle("active")
-  // );
-  // // show selected preset cover letter in input field
-  // optionsUl.addEventListener("click", (e) => {
-  //   sBtn_text.innerText = e.target.innerText;
-  //   optionMenu.classList.remove("active");
-  //   console.log(e.target.innerText);
-  //   // filter the cover letter by selected preset name
-  //   chrome.storage.sync.get("allPresets", (obj) => {
-  //     if (obj.allPresets) {
-  //       console.log(obj.allPresets);
-  //       let cover = obj.allPresets.filter(
-  //         (preset) => preset.presetName === e.target.innerText
-  //       );
-  //       console.log({ cover });
-  //       coverLetter.value = cover[0].presetDescription;
-  //     }
-  //   });
-  // });
-
-  // console.log(main);
-  // // show all the available preset in option list
-  // chrome.storage.sync.get("allPresets", (obj) => {
-  //   if (obj.allPresets) {
-  //     console.log(obj.allPresets);
-  //     obj.allPresets.map((preset) => {
-  //       let itemLi = document.createElement("li");
-  //       itemLi.className = "option";
-  //       itemLi.innerHTML = `<span class="option-text">${preset.presetName}</span>`;
-  //       optionsUl.appendChild(itemLi);
-  //       console.log(main);
-  //     });
-  //   } else {
-  //     // show 'no preset available text' in dropdown
-  //     optionsUl.innerText = "no preset available ";
-  //   }
-  // });
 })();
--- popup.js
+++ popup.js
@@ -1,74 +1,34 @@
-const createPresetInput = document.getElementById("preset_create_input");
+// set the preset in local storage when 'add' button is clicked
 const presetName = document.getElementById("preset_name");
 const presetDescription = document.getElementById("preset_description");
-const createPreset = document.getElementById("create_preset");
 const addPreset = document.getElementById("add_preset");
-const presetsDiv = document.getElementById("presets");
-console.log(createPresetInput);
-createPresetInput.style.display = "none";
-createPreset.style.display = "block";
-createPreset.addEventListener("click", function () {
-  createPresetInput.style.display = "block";
-  createPreset.style.display = "none";
+const managePreset = document.getElementById("manage_preset");
+const accWrapper = document.getElementsByClassName("accordionWrapper")[0];
+accWrapper.style.display = "none";
+managePreset.addEventListener("click", function () {
+  // accWrapper.style.display = "block";
+  if (accWrapper.style.display === "none") {
+    accWrapper.style.display = "block";
+  } else {
+    accWrapper.style.display = "none";
+  }
 });
-function presetElement(name, description) {
-  // create the div for preset element
-  console.log({ name });
-  console.log({ description });
-  const ElementParentDiv = document.createElement("div");
-  const presetNameButton = document.createElement("button");
-  presetNameButton.innerText = name;
-  const presetDescriptionP = document.createElement("p");
-  presetDescriptionP.innerText = description;
-  ElementParentDiv.appendChild(presetNameButton);
-  ElementParentDiv.appendChild(presetDescriptionP);
-  presetsDiv.appendChild(ElementParentDiv);
-  console.log(ElementParentDiv);
-  console.log(presetsDiv);
-}
-// show all presets on action clicked
-function showPreset() {
-  console.log("show func");
-  chrome.storage.sync.get("allPresets", (obj) => {
-    if (obj.allPresets) {
-      console.log("show presets", obj.allPresets);
-      // show presets in popup.html
-      obj.allPresets.map((preset) => {
-        const { presetName, presetDescription } = preset;
-        console.log(presetName);
-        console.log(presetDescription);
-        presetElement(presetName, presetDescription);
-      });
-      // presetsDiv.innerText = " preset available ";
-    } else {
-      // show 'no preset available text' in popup.html
-      presetsDiv.innerText = "no preset available now";
-    }
-  });
-}
-showPreset();
-// set the preset in local storage when 'add' button is clicked
 addPreset.addEventListener("click", function () {
   console.log("add clicked");
-  createPresetInput.style.display = "none";
-  createPreset.style.display = "block";
 
   chrome.storage.sync.get("allPresets", (obj) => {
     if (obj.allPresets) {
       console.log({ obj });
       let current = obj.allPresets;
-      chrome.storage.sync.set(
-        {
-          allPresets: [
-            ...current,
-            {
-              presetName: presetName.value,
-              presetDescription: presetDescription.value,
-            },
-          ],
-        },
-        showPreset(presetName.value, presetDescription.value)
-      );
+      chrome.storage.sync.set({
+        allPresets: [
+          ...current,
+          {
+            presetName: presetName.value,
+            presetDescription: presetDescription.value,
+          },
+        ],
+      });
     } else {
       console.log("its new");
       chrome.storage.sync.set({
@@ -81,63 +41,100 @@ addPreset.addEventListener("click", function () {
       });
     }
   });
-  // function onRemoved() {
-  //   console.log("OK");
-  // }
-
-  // function onError(e) {
-  //   console.log(e);
-  // }
-
-  // let removeKitten = chrome.storage.sync.remove("allPresets");
-  // removeKitten.then(onRemoved, onError);
 });
 
-// const createPresetInput = document.getElementById("preset_create_input");
-// const presetName = document.getElementById("preset_name");
-// const presetDescription = document.getElementById("preset_description");
-// const createPreset = document.getElementById("create_preset");
-// const addPreset = document.getElementById("add_preset");
-// console.log(createPresetInput);
-// createPresetInput.style.display = "none";
-// createPreset.style.display = "block";
-// createPreset.addEventListener("click", function () {
-//   createPresetInput.style.display = "block";
-//   createPreset.style.display = "none";
-// });
-// const fetchPresets = () => {
-//   return new Promise((resolve) => {
-//     chrome.storage.sync.get("allPresets", (obj) => {
-//       resolve(obj ? JSON.parse(JSON.stringify(obj)) : []);
-//     });
-//   });
-// };
-// addPreset.addEventListener("click", function () {
-//   console.log("add clicked");
-//   createPresetInput.style.display = "none";
-//   createPreset.style.display = "block";
-//   let currentPresets = [];
-//   let allPresets = "all";
-//   console.log(presetName.value);
-//   console.log(presetDescription.value);
+// get and show the presets
+chrome.storage.sync.get("allPresets", (obj) => {
+  if (obj.allPresets) {
+    // show all preset in accordion
+    const wrapper = document.querySelector(".accordionWrapper");
+    // create the accordion item
+    for (let index = 0; index < obj.allPresets.length; index++) {
+      const accItemDiv = document.createElement("div");
 
-//   const addNewPresetEventHandler = async () => {
-//     const newPreset = {
-//       presetName: presetName.value,
-//       presetDescription: presetDescription.value,
-//     };
+      accItemDiv.innerHTML = `<div class='${
+        index == 0 ? "accordionItem open" : "accordionItem close"
+      }'>
+                              <h2 
+                                class="accordionItemHeading" onclick='${toggleItem}' 
+                                style="display: flex;justify-content: space-between;"
+                              >
+                                <span> ${
+                                  obj.allPresets[index].presetName
+                                }</span>  
+                                
+                                  <img 
+                                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVvR826mwqgrKTSSKHwZLTm5NeW8kuJxGJzmp47Gf61bLbT771ClofEPJd7ERcB62njwI&usqp=CAU"
+                                    alt="delete" 
+                                    style="width:15px;height:15px"
+                                    class="accordionDelete"
+                                  />                            
+                              </h2>
+                              <div class="accordionItemContent">
+                                <button class="copy_cover_letter">copy</button>
+                                <p>
+                                ${obj.allPresets[index].presetDescription}
+                                </p>
+                              </div>
+                            </div>  `;
+      wrapper.appendChild(accItemDiv);
+    }
 
-//     currentPresets = await fetchPresets();
+    // add toggle func in all accordion item's header
+    const accItem = document.getElementsByClassName("accordionItem");
+    const accHD = document.getElementsByClassName("accordionItemHeading");
+    const accDelete = document.getElementsByClassName("accordionDelete");
+    const copyBtn = document.getElementsByClassName("copy_cover_letter");
+    for (let i = 0; i < accHD.length; i++) {
+      accHD[i].addEventListener("click", toggleItem, false);
+      accDelete[i].addEventListener("click", deleteItem, false);
+      copyBtn[i].addEventListener("click", copyText, false);
+    }
+    function toggleItem() {
+      const itemClass = this.parentNode.className;
+      for (let i = 0; i < accItem.length; i++) {
+        accItem[i].className = "accordionItem close";
+      }
+      if (itemClass == "accordionItem close") {
+        this.parentNode.className = "accordionItem open";
+      }
+    }
+    // delete a preset
+    function deleteItem(e) {
+      // grab the item
+      const selectedDeleteItem = e.target.parentNode.parentNode;
+      // item heading
+      const selectedHD = selectedDeleteItem.getElementsByClassName(
+        "accordionItemHeading"
+      )[0].innerText;
 
-//     await chrome.storage.sync.set(
-//       {
-//         allPresets: JSON.stringify([...currentPresets, newPreset]),
-//       },
-//       function (data) {
-//         console.log("Value is set to " + data);
-//         console.log("Value is " + JSON.stringify(currentPresets));
-//       }
-//     );
-//   };
-//   addNewPresetEventHandler();
-// });
+      // get the allpresets from storage then delete item by filtering  it and set new filtered presets
+      chrome.storage.sync.get("allPresets", (obj) => {
+        if (obj.allPresets) {
+          console.log({ obj });
+          let current = obj.allPresets;
+          const filteredPresets = current.filter((pre) => {
+            return pre.presetName !== selectedHD;
+          });
+          console.log({ filteredPresets });
+          chrome.storage.sync.set(
+            {
+              allPresets: filteredPresets,
+            },
+            function () {
+              selectedDeleteItem.remove();
+            }
+          );
+        }
+      });
+    }
+
+    // copy to clipboard
+    function copyText(e) {
+      const clickedCopyBtn =
+        e.target.parentNode.parentNode.getElementsByTagName("p")[0].innerText;
+      console.log({ clickedCopyBtn });
+      navigator.clipboard.writeText(clickedCopyBtn);
+    }
+  }
+});
