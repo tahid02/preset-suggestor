d6291dd98be0234b88313eb8d998db581d0ded35
tahid02 <tanzim1463@gmail.com>
2022-06-11 21:14:15 +0600

    just select preset btn responsiveness left

--- popup.js
+++ popup.js
@@ -20,41 +20,55 @@ addPreset.addEventListener("click", function () {
     if (obj.allPresets) {
       console.log({ obj });
       let current = obj.allPresets;
-      chrome.storage.sync.set({
-        allPresets: [
-          ...current,
-          {
-            presetName: presetName.value,
-            presetDescription: presetDescription.value,
-          },
-        ],
-      });
+      chrome.storage.sync.set(
+        {
+          allPresets: [
+            ...current,
+            {
+              presetName: presetName.value,
+              presetDescription: presetDescription.value,
+            },
+          ],
+        },
+        function () {
+          console.log("hello preset");
+          allPreset();
+        }
+      );
     } else {
       console.log("its new");
-      chrome.storage.sync.set({
-        allPresets: [
-          {
-            presetName: presetName.value,
-            presetDescription: presetDescription.value,
-          },
-        ],
-      });
+      chrome.storage.sync.set(
+        {
+          allPresets: [
+            {
+              presetName: presetName.value,
+              presetDescription: presetDescription.value,
+            },
+          ],
+        },
+        function () {
+          console.log("hello preset");
+          allPreset();
+        }
+      );
     }
   });
 });
-
-// get and show the presets
-chrome.storage.sync.get("allPresets", (obj) => {
-  if (obj.allPresets) {
-    // show all preset in accordion
+function allPreset() {
+  // get and show the presets
+  chrome.storage.sync.get("allPresets", (obj) => {
     const wrapper = document.querySelector(".accordionWrapper");
-    // create the accordion item
-    for (let index = 0; index < obj.allPresets.length; index++) {
-      const accItemDiv = document.createElement("div");
+    wrapper.innerHTML = "";
+    if (obj.allPresets.length) {
+      // show all preset in accordion
+      console.log(obj.allPresets);
+      // create the accordion item
+      for (let index = 0; index < obj.allPresets.length; index++) {
+        const accItemDiv = document.createElement("div");
 
-      accItemDiv.innerHTML = `<div class='${
-        index == 0 ? "accordionItem open" : "accordionItem close"
-      }'>
+        accItemDiv.innerHTML = `<div class='${
+          index == 0 ? "accordionItem open" : "accordionItem close"
+        }'>
                               <h2 
                                 class="accordionItemHeading" onclick='${toggleItem}' 
                                 style="display: flex;justify-content: space-between;"
@@ -72,69 +86,73 @@ chrome.storage.sync.get("allPresets", (obj) => {
                               </h2>
                               <div class="accordionItemContent">
                                 <button class="copy_cover_letter">copy</button>
-                                <p>
+                                <p style="font-size:1rem">
                                 ${obj.allPresets[index].presetDescription}
                                 </p>
                               </div>
                             </div>  `;
-      wrapper.appendChild(accItemDiv);
-    }
-
-    // add toggle func in all accordion item's header
-    const accItem = document.getElementsByClassName("accordionItem");
-    const accHD = document.getElementsByClassName("accordionItemHeading");
-    const accDelete = document.getElementsByClassName("accordionDelete");
-    const copyBtn = document.getElementsByClassName("copy_cover_letter");
-    for (let i = 0; i < accHD.length; i++) {
-      accHD[i].addEventListener("click", toggleItem, false);
-      accDelete[i].addEventListener("click", deleteItem, false);
-      copyBtn[i].addEventListener("click", copyText, false);
-    }
-    function toggleItem() {
-      const itemClass = this.parentNode.className;
-      for (let i = 0; i < accItem.length; i++) {
-        accItem[i].className = "accordionItem close";
+        wrapper.appendChild(accItemDiv);
       }
-      if (itemClass == "accordionItem close") {
-        this.parentNode.className = "accordionItem open";
-      }
-    }
-    // delete a preset
-    function deleteItem(e) {
-      // grab the item
-      const selectedDeleteItem = e.target.parentNode.parentNode;
-      // item heading
-      const selectedHD = selectedDeleteItem.getElementsByClassName(
-        "accordionItemHeading"
-      )[0].innerText;
 
-      // get the allpresets from storage then delete item by filtering  it and set new filtered presets
-      chrome.storage.sync.get("allPresets", (obj) => {
-        if (obj.allPresets) {
-          console.log({ obj });
-          let current = obj.allPresets;
-          const filteredPresets = current.filter((pre) => {
-            return pre.presetName !== selectedHD;
-          });
-          console.log({ filteredPresets });
-          chrome.storage.sync.set(
-            {
-              allPresets: filteredPresets,
-            },
-            function () {
-              selectedDeleteItem.remove();
-            }
-          );
+      // add toggle func in all accordion item's header
+      const accItem = document.getElementsByClassName("accordionItem");
+      const accHD = document.getElementsByClassName("accordionItemHeading");
+      const accDelete = document.getElementsByClassName("accordionDelete");
+      const copyBtn = document.getElementsByClassName("copy_cover_letter");
+      for (let i = 0; i < accHD.length; i++) {
+        accHD[i].addEventListener("click", toggleItem, false);
+        accDelete[i].addEventListener("click", deleteItem, false);
+        copyBtn[i].addEventListener("click", copyText, false);
+      }
+      function toggleItem() {
+        const itemClass = this.parentNode.className;
+        for (let i = 0; i < accItem.length; i++) {
+          accItem[i].className = "accordionItem close";
         }
-      });
-    }
+        if (itemClass == "accordionItem close") {
+          this.parentNode.className = "accordionItem open";
+        }
+      }
+      // delete a preset
+      function deleteItem(e) {
+        // grab the item
+        const selectedDeleteItem = e.target.parentNode.parentNode;
+        // item heading
+        const selectedHD = selectedDeleteItem.getElementsByClassName(
+          "accordionItemHeading"
+        )[0].innerText;
 
-    // copy to clipboard
-    function copyText(e) {
-      const clickedCopyBtn =
-        e.target.parentNode.parentNode.getElementsByTagName("p")[0].innerText;
-      console.log({ clickedCopyBtn });
-      navigator.clipboard.writeText(clickedCopyBtn);
+        // get the allpresets from storage then delete item by filtering  it and set new filtered presets
+        chrome.storage.sync.get("allPresets", (obj) => {
+          if (obj.allPresets) {
+            console.log({ obj });
+            let current = obj.allPresets;
+            const filteredPresets = current.filter((pre) => {
+              return pre.presetName !== selectedHD;
+            });
+            console.log({ filteredPresets });
+            chrome.storage.sync.set(
+              {
+                allPresets: filteredPresets,
+              },
+              function () {
+                selectedDeleteItem.remove();
+              }
+            );
+          }
+        });
+      }
+
+      // copy to clipboard
+      function copyText(e) {
+        const clickedCopyBtn =
+          e.target.parentNode.parentNode.getElementsByTagName("p")[0].innerText;
+        console.log({ clickedCopyBtn });
+        navigator.clipboard.writeText(clickedCopyBtn);
+      }
+    } else {
+      wrapper.innerHTML = `<h2 style="text-align:center"> "No preset added yet"</h2>`;
     }
-  }
-});
+  });
+}
+allPreset();
